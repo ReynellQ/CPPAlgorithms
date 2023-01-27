deccd432f862d1d8ae1e32686ae8d1f5d66671e2
ReynellQ <73651961+ReynellQ@users.noreply.github.com>
2023-01-27 09:25:53 -0500

    Added StringTokenizer

--- Java/Main.java
+++ Java/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
         in.close();
         out.flush();
         out.close();
