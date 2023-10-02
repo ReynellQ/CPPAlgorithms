a9cc2ff77133fa0b0305feee8a27268285548503
dhliv <dvdhenaomartinez@gmail.com>
2023-10-02 09:59:35 -0500

    Adapted arituclation points to consider repeated nodes

--- C++/Grafos/articulations.cpp
+++ C++/Grafos/articulations.cpp
@@ -1,7 +1,7 @@
 int n;
 vector<vector<int>> g;
 
-vector<bool> visited, marked;
+vector<bool> visited;
 vector<int> tin, low;
 int timer;
 
@@ -16,24 +16,21 @@ void dfs(int v, int p = -1) {
     else {
       dfs(to, v);
       low[v] = min(low[v], low[to]);
-      if (low[to] >= tin[v] && p != -1 and !marked[v]){
+      if (low[to] >= tin[v] && p != -1){
         IS_CUTPOINT(v);
-        marked[v] = 1;
       }
       ++children;
     }
   }
 
-  if(p == -1 && children > 1 and !marked[i]){
+  if(p == -1 && children > 1){
     IS_CUTPOINT(v);
-    marked[v] = 1;
   }
 }
 
 void find_cutpoints() {
   timer = 0;
   visited.assign(n, false);
-  marked.assign(n, false);
   tin.assign(n, -1);
   low.assign(n, -1);
   for (int i = 0; i < n; ++i) {
